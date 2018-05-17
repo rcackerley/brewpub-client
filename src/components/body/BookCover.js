70c3edec71fa467936f1d58ded6b052cfce424f5
Robby Ackerley <rcackerley@me.com>
2018-05-16 21:40:27 -0400

    added search functionality for books and beers

--- src/components/body/BookCover.js
+++ src/components/body/BookCover.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
-let BookCover = ({img}) =>
-  <img alt="Book Cover" className="cover" src={img} />
+let BookCover = ({img, icon}) =>
+  <Fragment>
+    <img alt="Book Cover" className="cover" src={img} />
+    <img alt="Book Cover" className="shelf-beer" src={icon} />
+  </Fragment>
 
 
 export default BookCover;
